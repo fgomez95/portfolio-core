d1becadf55de74238f1a0a0509806548d46690ef
Fernando Gomez <fgomez.sierra95@gmail.com>
2019-06-22 14:39:25 -0600

    generate ticket model, installed factory and db cleaner

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,13 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_06_21_015401) do
+ActiveRecord::Schema.define(version: 2019_06_22_201053) do
+
+  create_table "tickets", force: :cascade do |t|
+    t.string "description"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "users", force: :cascade do |t|
     t.string "email", default: "", null: false
